0f6648a3288d119a034bb9752914738b9cb1b640
sharejia <sharejia@outlook.com>
2019-04-10 13:38:04 +0800

    edit

---
+++ Day_Exam/PMP/Day20.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @Author: Sharejia
+ * @E-mail: sharejia@outlook.com
+ * @Date:   2019-04-03 18:55:23
+ * @Last Modified by:   贾鑫晨
+ * @Last Modified time: 2019-04-03 18:57:08
+ */
+class Day20_exam{
+
+	public function __construct(){
+		$res = $this->com(10,10);
+	}
+
+	public function com($x = 0,$y = 0){
+		
+	}
+
+
+}
+new Day20_exam();
